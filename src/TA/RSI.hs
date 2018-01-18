6899e1297bec415b959cce7927f0a29a8d83714c
epicallan <epicallan.al@gmail.com>
2018-01-18 11:13:26 +0300

    blocked out RSI calculation module

--- src/TA/RSI.hs
+++ src/TA/RSI.hs
@@ -5,27 +5,53 @@ import           Data.Maybe
 import           Protolude
 
 
+type Period = Int
+data AverageType = Gain | Loss deriving (Ord, Eq, Show)
+
 pairUp :: Fractional a => [a] -> Maybe [(a,a)]
 pairUp []        = Just []
 pairUp [x]       = Nothing
 pairUp (x:x':xs) =  (:) <$> Just (x,x') <*> pairUp xs
 
 
-sumPairs :: (Fractional a, Ord a) => Maybe [(a, a)] -> Maybe a
-sumPairs pairs = case pairs of
+pairDiff :: (Fractional a, Ord a)  => AverageType -> (a, a) -> a
+pairDiff avType (x, y)
+    | avType == Gain && diff > 0 = diff
+    | avType == Loss && diff < 0 = diff * (- 1)
+    | otherwise = 0
+    where diff = y - x
+
+sumPairs :: (Fractional a, Ord a) => AverageType -> Maybe [(a, a)] -> Maybe a
+sumPairs avType pairs = case pairs of
     Just xs -> Just $ sumOfPairs xs
     Nothing -> Nothing
     where
-        sumOfPairs = foldr (\tupel acc -> let (x, y) = tupel
-                                              gain = y - x
-                                          in if gain > 0
-                                                then gain + acc
-                                                else 0
-                                    ) 0
+        sumOfPairs = foldr (\pair acc -> acc + pairDiff avType pair) 0
 
 
-averageGain :: (Fractional a, Ord a) => Int -> [a] -> Maybe a
-averageGain period values = div'' <$> totalGains
+-- for getting first averages & losses
+firstAverage :: (Fractional a, Ord a) => AverageType -> Int -> Maybe [(a, a)] -> Maybe a
+firstAverage avType period pairs = div'' <$> totalGains
     where
-        totalGains =  sumPairs $ pairUp $ take period values
+        totalGains = sumPairs avType $ fmap (take period) pairs
         div'' = flip (/) $ fromIntegral period
+
+
+foldAverages :: (Fractional a, Ord a) => AverageType -> Period -> a ->[(a, a)] -> a
+foldAverages avType period =
+    foldr(\pair acc -> let currentDiff = pairDiff avType pair
+                        in (acc * 13 + currentDiff) / fromIntegral period)
+
+aggregateAverages :: (Fractional a, Ord a) => AverageType -> Period -> [a] -> Maybe a
+aggregateAverages avType period values =
+    let allPairs = pairUp values
+        initialAverage = firstAverage avType period allPairs
+    in case initialAverage of
+        Just avg -> foldAverages avType period avg <$> allPairs
+        Nothing  -> Nothing
+
+averageGain :: (Fractional a, Ord a) => Period -> [a] -> Maybe a
+averageGain = aggregateAverages Gain
+
+averageLoss :: (Fractional a, Ord a) => Period -> [a] -> Maybe a
+averageLoss = aggregateAverages Loss
